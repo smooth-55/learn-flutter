0b0f37ecac3bdb682a575ddde87df91ae4a70540
Ashish <dhakalashish18@gmail.com>
2023-09-29 10:36:56 +0545

    feat: home

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:todo/screens/home.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.lightGreen),
+    );
+  }
+}
+
+
